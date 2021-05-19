6162bdc272a848c6e1c1defa05a25b563d336bbe
slecaros <slecaros@uc.cl>
2021-05-19 01:15:26 -0400

    get catalogue: all, by id or by shop

--- my_app/routes/product.py
+++ my_app/routes/product.py
@@ -1,3 +1,4 @@
+from my_app.models import shop
 from my_app import app, db, cache
 from flask import jsonify, request, make_response
 from ..models.product import Product
@@ -22,3 +23,26 @@ def create_product():
     db.session.commit()
     product = Product.load_product(str(id))
     return make_response(jsonify(product), 201)
+
+
+@app.route('/catalogue', methods=['GET'])
+@cache.cached(timeout=50)
+def get_catalogue():
+    unserialized_products = Product.query.all()
+    serialized_products = [product.serialize()
+                           for product in unserialized_products]
+    return make_response(jsonify(serialized_products))
+
+
+@app.route('/catalogue/product', methods=['GET'])
+def get_product():
+    product = Product.query.get(str(request.args.get('productId')))
+    if product:
+        return make_response(jsonify(product.serialize()))
+    return {}
+
+
+@app.route('/catalogue/shop', methods=['GET'])
+def get_shop_catalogue():
+    products = Product.load_shops_products(request.args.get('shopId'))
+    return make_response(jsonify(products))
